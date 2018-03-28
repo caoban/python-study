d17c59e63518eda7d8075ab7b8ed81993a46311f
xiaosuhan <752808409@qq.com>
2018-03-28 22:09:04 +0800

    day06 析构函数 私有变量 类变量实例变量

--- day06/OOP开枪.py
+++ day06/OOP开枪.py
@@ -1,10 +1,18 @@
 class Role(object):
+    n = 123 #类变量
+    a_list = []
     def __init__(self,name,role,weapon,life_value=100,money=15000):
-        self.name = name
+        self.name = name    #实例变量
         self.role = role
         self.weapon = weapon
-        self.life_value = life_value
+        self.__life_value = life_value
         self.money = money
+
+    def show_lifevalue(self):
+        print("life_value:%s" %self.__life_value)
+
+    def __del__(self):
+        pass #print("%s 彻底死了。。。。")
 
     def shot(self):
         print("shooting。。。。。")
@@ -17,4 +25,27 @@ class Role(object):
 role1 = Role('Alex','police','AK47') #生成一个角色
 role2 = Role('jack','terrorist','B22') #生成一个角色
 
-role1.buy_gun("AK47")
+print(role1.show_lifevalue())
+
+
+
+    #可以改类变量
+role1.n = ("改类变量")
+
+#     #role1实例中没有改
+# print(role1.n)
+#     #role2 实例中没有改
+# print(role2.n)
+
+# Role.n = "ABC"
+# print(role1.n,role2.n)
+#
+# role1.a_list.append("from r1")
+# role2.a_list.append("from r2")
+#
+# print("role1 list: %s" %role1.a_list)
+# print("role2 list: %s" %role2.a_list)
+# print("Role类 list: %s" %Role.a_list)
+
+
+
