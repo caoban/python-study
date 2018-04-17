bc54ba6f6fe09bb9915326135d7cd2510f9b6654
xiaosuhan <752808409@qq.com>
2018-04-17 22:21:50 +0800

    add

--- day07/socket_client.py
+++ day07/socket_client.py
@@ -3,12 +3,17 @@ import socket
 
 client = socket.socket() #声明socket类型，同时生成socket连接对象
 client.connect(('localhost',6969))
+while True:
+    msg = input(">>:").strip()
+    if len(msg) == 0:continue
+    client.send(msg.encode("utf-8"))
+    data = client.recv(1024)
+    print("recv:" ,data.decode())
 
-client.send(b"Hello World!")
-data = client.recv(1024)
-
-print("recv:" ,data)
 client.close()
+
+
+
 
 
 
--- day07/socket_server.py
+++ day07/socket_server.py
@@ -5,16 +5,20 @@ server.bind(('localhost',6969)) #绑定监听的端口
 server.listen() #监听
 
 print("我要开始等电话了")
-conn,addr = server.accept() #等电话打进来
-#conn 就是客户端连接过来，而在服务器端为其生成的一个连接实例。
-print(conn,addr)
 
-print("电话来了")
+while True:
+    conn,addr = server.accept() #等电话打进来
+    # conn 就是客户端连接过来，而在服务器端为其生成的一个连接实例。
+    print(conn, addr)
+    print("电话来了")
 
-data = conn.recv(1024)
-print("recv:",data)
-
-conn.send(data.upper())
+    while True:
+        data = conn.recv(1024)
+        print("recv:",data)
+        if not data:
+            print("client has lost....")
+            break
+        conn.send(data.upper())
 
 server.close()
 
