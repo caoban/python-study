4e1f8591a369d972362db5df9018e9128397ce94
han.su <han.su@guanaitong.com>
2018-04-20 09:36:27 +0800

    update

--- scripts/redis_mysql.py
+++ scripts/redis_mysql.py
@@ -2,14 +2,6 @@
 import redis
 import pymysql
 
-#连接redis
-pool = redis.ConnectionPool(host='10.101.11.107', port=6379, db=3)
-r = redis.Redis(connection_pool=pool)
-
-# r.set('name', 'test')
-# print(r.get('name'))
-
-
 
 # 打开数据库连接
 db = pymysql.connect(
@@ -23,16 +15,23 @@ db = pymysql.connect(
 # 使用 cursor() 方法创建一个游标对象 cursor
 cursor = db.cursor()
 
-# 使用 execute()  方法执行 SQL 查询
-cursor.execute("show databases")
 
-# 使用 fetchone() 方法获取单条数据.
-data = cursor.fetchone()
+#连接redis
+pool = redis.ConnectionPool(host='10.101.11.107', port=6379, db=3)
+r = redis.Redis(connection_pool=pool)
+#获取所有的key
+keys = r.keys()
+for key in keys:
+    value = r.get('ip1').decode()
+    # 使用 execute()  方法执行 SQL 查询
+    cursor.execute("")
+    # 使用 fetchone() 方法获取单条数据.
+    data = cursor.fetchone()
 
-print("Database version : %s " % data)
 
-# 关闭数据库连接
-db.close()
+#
+# # 关闭数据库连接
+# db.close()
 
 
 
