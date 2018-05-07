ef278b010398c1fc0e30310739d1fba876b454f8
xiaosuhan <752808409@qq.com>
2018-05-07 22:08:38 +0800

    choise course

---
+++ day06/chose_course/Course.py
@@ -0,0 +1,21 @@
+
+#课程类
+class Course(object):
+    pass
+
+
+#班级类
+class Classroom(object):
+    pass
+
+#学校成员类
+class SchoolMember(object):
+    pass
+
+#学生类
+class Student(SchoolMember):
+    pass
+
+#讲师类
+class Teacher(SchoolMember):
+    pass
---
+++ day06/chose_course/School.py
@@ -0,0 +1,18 @@
+import sys
+
+#学校类
+class School(object):
+    #定义静态方法
+    def __init__(self,SchoolName,Address):
+        self.SchoolName = SchoolName
+        self.Address = Address
+    #创建班级类
+    def CreateClass(self):
+        pass
+
+    def HireTeacher(self):
+        pass
+
+    def CreateCourse(self):
+        pass
+
