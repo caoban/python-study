ec41235bed67b13b7f81c609747964d552c97da4
han.su <han.su@guanaitong.com>
2018-05-16 16:53:14 +0800

    update

--- scripts/gitlabregister/GitlabRegister.py
+++ scripts/gitlabregister/GitlabRegister.py
@@ -13,38 +13,42 @@ import string
 url = 'http://gitlab.wuxingdev.cn'
 token = 'zsm44PTux_r65p5rbTBG'
 
-#登录
-#gl = gitlab.Gitlab(url,private_token='token')
+#拼接创建用户的url
 CreateUserUrl = "%s/api/v3/users" %url
+EmailExample = "guanaitong.com"
 
-#输入需要创建的用户信息，密码是随机生成的
-name = input("请输入中文用户名:")
-email = input("请输入邮箱地址:")
-username = email.strip().split("@guanaitong.com")[0]
-password = "".join(random.sample(['A','b','5','D','e','7','g','h','2','j','q'], 8))
+while True:
+    # 输入需要创建的用户信息，密码是随机生成的
+    name = input("请输入中文用户名:")
+    email = input("请输入邮箱地址:")
+    username = email.strip().split("@guanaitong.com")[0]
+    password = "".join(random.sample(['A', 'b', '5', 'D', 'e', '7', 'g', 'h', '2', 'j', 'q'], 8))
+    # print("输入信息为：\n中文用户名:%s\n邮箱地址:%s\n登录username:%s\n" %(name,email,username))
 
-print("name:%s\nemail:%s\nusername:%s\npassword:%s\n" %(name,email,username,password))
+    if email.strip().split("@")[1] != EmailExample:
+        print("邮箱格式不对!%s" %email)
+        continue
+    else:
+        UserData = {'email': email, 'username': username, 'name': name, 'password': password, 'private_token': token}
+        response = requests.post(CreateUserUrl, data=UserData)
+        if response.status_code == 201:
+            email2 = "han.su@guanaitong.com"
+            data2 = {'tos':email2,'subject':"Gitlab密码",'content':"gitlab地址：" + url + " 登录用户：" + username + " 登录邮箱：" + email + " 密码：" + password }
+            EmailUrl = "http://notice.ops.gat/sender/mail/"
+            SendEmailResponse = requests.post(EmailUrl,data = data2)
+            print("用户创建成功，并将密码发给用户")
+            input("请输入任意字符退出:")
+            break
+        else:
+            print("创建失败，请确认邮箱和用户名是否已存在")
+            continue
 
-UserData = {'email':email,'username':username,'name':name,'password':password,'private_token':token}
-response = requests.post(CreateUserUrl, data = UserData)
-
-print("response.status_code%s\n:" %response.status_code)
-
-if response.status_code == 201:
-    print("创建成功")
-    email2 = "han.su@guanaitong.com"
-    data2 = {'tos':email2,'subject':"Gitlab密码",'content':password}
-    EmailUrl = "https://notice.ops.gat/sender/mail/"
-    SendEmailResponse = requests.post(EmailUrl,data = data2)
-else:
-    print("创建失败，请确认邮箱和用户名是否已存在")
 
 
-
+#http://python-gitlab.readthedocs.io/en/stable/api-usage.html
 
 #xurl = "%s/api/v3/projects?private_token=%s" %(url,token)
 # x = requests.get(xurl)
-# print(x)
 # #两种都可以
 # content = x.json()
 # content2 = x.text
