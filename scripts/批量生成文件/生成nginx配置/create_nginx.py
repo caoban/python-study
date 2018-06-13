65cea2b6f67e41804e2d45a4c9782d4a65a0e0a7
han.su <han.su@guanaitong.com>
2018-06-13 22:25:28 +0800

    测试pod中访问远程端口通不通

--- scripts/批量生成文件/生成nginx配置/create_nginx.py
+++ scripts/批量生成文件/生成nginx配置/create_nginx.py
@@ -1,18 +1,18 @@
 
 import re
 
-with open("domain02",'r') as f:
+with open("phpweb_domain",'r') as f:
     for line in f:
         line = line.strip()
 
 
         if re.search(".+guanaitong$",line) :
-
-            with open("testmall.guanaitong.cc.conf",'r') as fg:
-                for line_fg in fg:
-                    line_fgw = line_fg.replace("mall.guanaitong",line)
-                    with open(line + ".cc.conf", 'a+') as  fgw:
-                        fgw.write(line_fgw)
+            pass
+            # with open("testmall.guanaitong.cc.conf",'r') as fg:
+            #     for line_fg in fg:
+            #         line_fgw = line_fg.replace("mall.guanaitong",line)
+            #         with open(line + ".cc.conf", 'a+') as  fgw:
+            #             fgw.write(line_fgw)
 
 
         elif re.search(".+ciicgat$",line) :
@@ -24,12 +24,12 @@ with open("domain02",'r') as f:
                         fcw.write(line_fcw)
 
         elif re.search(".+4008885818$",line) :
-
-            with open("testmall.4008885818.cc.conf",'r') as f4:
-                for line_f4 in f4:
-                    line_f4w = line_f4.replace("mall.4008885818",line)
-                    with open(line + ".cc.conf", 'a+') as  f4w:
-                        f4w.write(line_f4w)
+            pass
+            # with open("testmall.4008885818.cc.conf",'r') as f4:
+            #     for line_f4 in f4:
+            #         line_f4w = line_f4.replace("mall.4008885818",line)
+            #         with open(line + ".cc.conf", 'a+') as  f4w:
+            #             f4w.write(line_f4w)
 
         else:
             #pass
