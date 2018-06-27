3248c50dd9682c94653e363b1011b37849556c3f
han.su <han.su@guanaitong.com>
2018-06-27 18:42:43 +0800

    ssh 登录

--- day09/ssh_rsaa.py
+++ day09/ssh_rsaa.py
@@ -1,16 +1,19 @@
 #导入包
 import paramiko
 
+#指定本地公钥位置
+private_key = paramiko.RSAKey.from_private_key_file('id_rsa.txt')
+
 #创建ssh对象
 ssh = paramiko.SSHClient()
 #允许连接不在know_hosts文件中的主机
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 
 #连接服务器
-ssh.connect(hostname='10.101.11.201',port=22,username='root',password='jifenpay')
+ssh.connect(hostname='10.101.11.201',port=22,username='root', pkey=private_key)
 #执行命令
 #stdin是标准输入 stdout是标准输出 stderr是标准错误
-stdin, stdout, stderr = ssh.exec_command('df -s')
+stdin, stdout, stderr = ssh.exec_command('df -h;hostname')
 
 #获取命令结果
 #三元运算，这样正确的和错误的结果都能获取到。
--- scripts/测试pod访问基础服务是否通/测试阿里云基础服务是否通.py
+++ scripts/测试pod访问基础服务是否通/测试阿里云基础服务是否通.py
@@ -1,3 +1,66 @@
-####
+
+import socket
+
+def CheckMysqlPort():
+    sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sk.settimeout(1)
+    try:
+      sk.connect(('mdb.servers.product.ali',3306))
+      print('mdb.servers.product.ali port 3306 OK!')
+    except Exception:
+      print('mdb.servers.product.ali port 3306 not connect!')
+    sk.close()
+
+def CheckRedisPort():
+    sk2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sk2.settimeout(1)
+    try:
+      sk2.connect(('192.168.96.4',6379))
+      print('redis 192.168.96.4 port 6379 OK!')
+    except Exception:
+      print('redis 192.168.96.4 port 6379 not connect!')
+    sk2.close()
+
+
+def CheckZkPort():
+    sk3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sk3.settimeout(1)
+    try:
+      sk3.connect(('192.168.96.1',2181))
+      print('zk 192.168.96.1 port 2181 OK!')
+    except Exception:
+      print('zk 192.168.96.1 port 2181 not connect!')
+    sk3.close()
+
+def CheckSLB80Port():
+    sk4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sk4.settimeout(1)
+    try:
+      sk4.connect(('192.168.64.4',80))
+      print('alislb 192.168.64.4 port 80 OK!')
+    except Exception:
+      print('alislb 192.168.64.4 port 80 not connect!')
+    sk4.close()
+
+def CheckSLB443Port():
+    sk4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sk4.settimeout(1)
+    try:
+      sk4.connect(('192.168.64.4',443))
+      print('alislb 192.168.64.4 port 443 OK!')
+    except Exception:
+      print('alislb 192.168.64.4 port 443 not connect!')
+    sk4.close()
+
+
+
+
+if __name__ == "__main__":
+    CheckMysqlPort()
+    CheckRedisPort()
+    CheckZkPort()
+    CheckSLB80Port()
+    CheckSLB443Port()
+    
 
 
